112131edfc34e42c8c0bfef98c1248ebe9747dbc
Franco carrasco <franco.carrasco.obreque@alumnos.sip.cl>
2022-05-12 12:26:34 -0400

    aaaaaaa

--- index.js
+++ index.js
@@ -18,42 +18,52 @@ var worldDict = {
     2: "sushi"
 }
 function drawWorld(){
-    var output = "";
+    output = "";
             
     for(var row = 0; row < world.length; row++){
-        output += "<div class = 'row'></div>"
+        output += "<div class = 'row'>"
             for(var x = 0; x < world[row].length; x++){
                 output += "<div class = '" + worldDict[
                 world[row][x]] +"'></div>"
             }
         output += "</div>"
     }       
-    document.getElementById("world").innerHTML = output;
+    document.getElementById('world').innerHTML = output;
 }
-drawWorld();{
-    var ninjaman = {
-        x:1,
-        y:1
-    }
-    function drawNinjaman(){
-            document.getElementById('ninjaman').style.top =
-            ninjaman.x * 40 + 'px'
-            document.getElementById('ninjaman').style.left =
-            ninjaman.x * 40 + 'px'
-        }
+drawWorld();
+
+var ninjaman = {
+    x: 2,
+    y: 6
 }
-            
+
+function drawNinjaman(){
+    document.getElementById('ninjaman').style.top = ninjaman.y * 40 + 'px'
+    document.getElementById('ninjaman').style.left = ninjaman.x * 40 + 'px'
+}
 drawNinjaman()
 
 document.onkeydown = function(e){
-    if(e.keycode == 37){
-        ninjaman.x--;
+    if(e.keyCode == 37){
+        if(world[ninjaman.y][ninjaman.x - 1] !=1){
+            ninjaman.x--;
+        }
     }
-    if(e.keycode == 39){
-        ninjaman.x++;
+    if(e.keyCode == 39){
+        if(world[ninjaman.y][ninjaman.x + 1] !=1){
+            ninjaman.x++;
+        }
     }
-    if(e.keycode == 40){
-        ninjaman.y--;
+    if(e.keyCode == 38){
+        if(world[ninjaman.y - 1][ninjaman.x] !=1){
+            ninjaman.y--;
+        }
     }
+    if(e.keyCode == 40){
+        if(world[ninjaman.y + 1][ninjaman.x] !=1){
+            ninjaman.y++;
+        }
+    }
+    world[ninjaman.y][ninjaman.x] = 0;
     drawNinjaman()
 }
